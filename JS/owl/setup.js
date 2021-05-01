32373a3f3882710677aacc1477ff94673b532531
Alexsouzabk <81489451+Alexsouzabk@users.noreply.github.com>
2021-05-01 09:35:36 -0300

    Add files via upload

---
+++ JS/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
